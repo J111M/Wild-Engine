7035685d4d187f6098d5c35c1485403eb9effa6a
Jim <jimvdpiep@gmail.com>
2026-03-17 16:31:27 +0100

    [Add] New GetOrCreateResource function that add a new resource to the resource system

--- Wild/Systems/ResourceSystem.hpp
+++ Wild/Systems/ResourceSystem.hpp
@@ -1,19 +1,28 @@
 #pragma once
 
+#include <memory>
 #include <unordered_map>
 
 namespace Wild
 {
-    template<typename Resource>
-    class ResourceSystem
+    template<typename Resource> 
+    class ResourceSystem : public NonCopyable
     {
       public:
         ResourceSystem();
         ~ResourceSystem();
 
-        Resource GetOrCreateResource(std::string key);
+        template <typename... Args> 
+        std::shared_ptr<Resource> GetOrCreateResource(std::string key, Args&&... args);
+
+        template <typename... Args>
+        std::shared_ptr<Resource> CreateResource(std::string key, Args&&... args);
+
+        std::shared_ptr<Resource> GetResource(std::string key);
+        
 
       private: 
           std::unordered_map<std::string, Resource> m_resource{};
     };
-}
+ 
+} // namespace Wild
