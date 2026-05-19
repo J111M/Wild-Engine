2f7829f9c81c6e552b97ebf4b6b296bd156b98e3
Jim <jimvdpiep@gmail.com>
2026-05-19 09:43:15 +0200

    [Change] Turned raytracing off by default since the output is albedo
    - To turn on raytracing press the O key in release

--- Wild/Renderer/Passes/DDGIPass.cpp
+++ Wild/Renderer/Passes/DDGIPass.cpp
@@ -21,76 +21,82 @@ namespace Wild
 
         rg.AddPass<DDGIPassData>(
             "DDGI pass", PassType::Raytracing, [&renderer, desc, this](DDGIPassData& passData, CommandList& list) {
-                PipelineStateSettings settings{};
-
-                settings.ShaderState.rayTracingShader =
-                    engine.GetShaderTracker()->GetOrCreateShader("Shaders/DDGI/ddgiRaytrace.slang");
-
-                settings.raytracingState.payloadSize = 44;
-                settings.raytracingState.attributeSize = sizeof(float) * 2;
-
-                settings.raytracingState.rayRecursionDepth = 3;
-
-                std::vector<Uniform> uniforms;
-
-                Uniform rootConstant{0, 0, RootParams::RootResourceType::Constants, sizeof(DDGIRC)};
-                uniforms.emplace_back(rootConstant);
-
-                Uniform accelerationStructure{0, 0, RootParams::RootResourceType::ShaderResourceView};
-                uniforms.emplace_back(accelerationStructure);
-
-                // Output render target
-                Uniform outputTexture{0, 0, RootParams::RootResourceType::DescriptorTable};
-                CD3DX12_DESCRIPTOR_RANGE outputUAVRange{};
-                outputUAVRange.Init(D3D12_DESCRIPTOR_RANGE_TYPE_UAV, 1, 0, 0, D3D12_DESCRIPTOR_RANGE_FLAG_NONE);
-                outputTexture.ranges.emplace_back(outputUAVRange);
-                uniforms.emplace_back(outputTexture);
-
-                Uniform meshInfoBuffer{1, 0, RootParams::RootResourceType::ShaderResourceView};
-                uniforms.emplace_back(meshInfoBuffer);
-
-                Uniform bindlessHeap{0, 1, RootParams::RootResourceType::DescriptorTable};
-                CD3DX12_DESCRIPTOR_RANGE bufRange{};
-                bufRange.Init(D3D12_DESCRIPTOR_RANGE_TYPE_SRV, UINT_MAX, 0, 1, D3D12_DESCRIPTOR_RANGE_FLAG_DESCRIPTORS_VOLATILE);
-                CD3DX12_DESCRIPTOR_RANGE texRange{};
-                texRange.Init(D3D12_DESCRIPTOR_RANGE_TYPE_SRV, UINT_MAX, 0, 2, D3D12_DESCRIPTOR_RANGE_FLAG_DESCRIPTORS_VOLATILE);
-                CD3DX12_DESCRIPTOR_RANGE cubeTexRange{};
-                cubeTexRange.Init(
-                    D3D12_DESCRIPTOR_RANGE_TYPE_SRV, UINT_MAX, 0, 3, D3D12_DESCRIPTOR_RANGE_FLAG_DESCRIPTORS_VOLATILE);
-                bindlessHeap.ranges.emplace_back(bufRange);
-                bindlessHeap.ranges.emplace_back(texRange);
-                bindlessHeap.ranges.emplace_back(cubeTexRange);
-                uniforms.emplace_back(bindlessHeap);
-
-                Uniform staticSampler{0, 0, RootParams::RootResourceType::StaticSampler};
-                uniforms.emplace_back(staticSampler);
-
-                auto& pipeline = renderer.GetOrCreatePipeline(
-                    "Dynamic Diffuse Global Illumination Pass", PipelineStateType::Raytracing, settings, uniforms);
-                list.SetPipelineState(pipeline);
-                list.BeginRender("Dynamic Diffuse Global Illumination pass");
-
-                if (renderer.environmentMap) m_rc.environmentView = renderer.environmentMap->GetSrv()->BindlessView();
-
-                list.SetRootConstant<DDGIRC>(0, m_rc);
-                list.GetList()->SetComputeRootShaderResourceView(1, engine.GetAccelerationStructureManager()->GetTLASAddress());
-                list.SetUnorderedAccessView(2, passData.finalTex);
-                list.SetShaderResourceView(3, engine.GetAccelerationStructureManager()->GetMeshIdBuffer().get());
-                list.SetBindlessHeap(4);
-
-                // Modify to probe size
-                list.Dispatch(desc.width, desc.height, 1);
-
-                list.EndRender();
-
-                passData.finalTex->Transition(list, D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE);
-
                 // TODO remove temporary bool
-                static bool s_rtEnabled = true;
+                static bool s_rtEnabled = false;
 
                 if (ImGui::IsKeyPressed(ImGuiKey_O, false)) { s_rtEnabled = !s_rtEnabled; }
 
-                if (s_rtEnabled) renderer.compositeTexture = passData.finalTex;
+                if (s_rtEnabled)
+                {
+                    PipelineStateSettings settings{};
+
+                    settings.ShaderState.rayTracingShader =
+                        engine.GetShaderTracker()->GetOrCreateShader("Shaders/DDGI/ddgiRaytrace.slang");
+
+                    settings.raytracingState.payloadSize = 44;
+                    settings.raytracingState.attributeSize = sizeof(float) * 2;
+
+                    settings.raytracingState.rayRecursionDepth = 3;
+
+                    std::vector<Uniform> uniforms;
+
+                    Uniform rootConstant{0, 0, RootParams::RootResourceType::Constants, sizeof(DDGIRC)};
+                    uniforms.emplace_back(rootConstant);
+
+                    Uniform accelerationStructure{0, 0, RootParams::RootResourceType::ShaderResourceView};
+                    uniforms.emplace_back(accelerationStructure);
+
+                    // Output render target
+                    Uniform outputTexture{0, 0, RootParams::RootResourceType::DescriptorTable};
+                    CD3DX12_DESCRIPTOR_RANGE outputUAVRange{};
+                    outputUAVRange.Init(D3D12_DESCRIPTOR_RANGE_TYPE_UAV, 1, 0, 0, D3D12_DESCRIPTOR_RANGE_FLAG_NONE);
+                    outputTexture.ranges.emplace_back(outputUAVRange);
+                    uniforms.emplace_back(outputTexture);
+
+                    Uniform meshInfoBuffer{1, 0, RootParams::RootResourceType::ShaderResourceView};
+                    uniforms.emplace_back(meshInfoBuffer);
+
+                    Uniform bindlessHeap{0, 1, RootParams::RootResourceType::DescriptorTable};
+                    CD3DX12_DESCRIPTOR_RANGE bufRange{};
+                    bufRange.Init(
+                        D3D12_DESCRIPTOR_RANGE_TYPE_SRV, UINT_MAX, 0, 1, D3D12_DESCRIPTOR_RANGE_FLAG_DESCRIPTORS_VOLATILE);
+                    CD3DX12_DESCRIPTOR_RANGE texRange{};
+                    texRange.Init(
+                        D3D12_DESCRIPTOR_RANGE_TYPE_SRV, UINT_MAX, 0, 2, D3D12_DESCRIPTOR_RANGE_FLAG_DESCRIPTORS_VOLATILE);
+                    CD3DX12_DESCRIPTOR_RANGE cubeTexRange{};
+                    cubeTexRange.Init(
+                        D3D12_DESCRIPTOR_RANGE_TYPE_SRV, UINT_MAX, 0, 3, D3D12_DESCRIPTOR_RANGE_FLAG_DESCRIPTORS_VOLATILE);
+                    bindlessHeap.ranges.emplace_back(bufRange);
+                    bindlessHeap.ranges.emplace_back(texRange);
+                    bindlessHeap.ranges.emplace_back(cubeTexRange);
+                    uniforms.emplace_back(bindlessHeap);
+
+                    Uniform staticSampler{0, 0, RootParams::RootResourceType::StaticSampler};
+                    uniforms.emplace_back(staticSampler);
+
+                    auto& pipeline = renderer.GetOrCreatePipeline(
+                        "Dynamic Diffuse Global Illumination Pass", PipelineStateType::Raytracing, settings, uniforms);
+                    list.SetPipelineState(pipeline);
+                    list.BeginRender("Dynamic Diffuse Global Illumination pass");
+
+                    if (renderer.environmentMap) m_rc.environmentView = renderer.environmentMap->GetSrv()->BindlessView();
+
+                    list.SetRootConstant<DDGIRC>(0, m_rc);
+                    list.GetList()->SetComputeRootShaderResourceView(1,
+                                                                     engine.GetAccelerationStructureManager()->GetTLASAddress());
+                    list.SetUnorderedAccessView(2, passData.finalTex);
+                    list.SetShaderResourceView(3, engine.GetAccelerationStructureManager()->GetMeshIdBuffer().get());
+                    list.SetBindlessHeap(4);
+
+                    // Modify to probe size
+                    list.Dispatch(desc.width, desc.height, 1);
+
+                    list.EndRender();
+
+                    passData.finalTex->Transition(list, D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE);
+
+                    renderer.compositeTexture = passData.finalTex;
+                }
             });
     }
 
