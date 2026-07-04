272a687492b6c8510db28a63dc448e2726c542ab
Jim <jimvdpiep@gmail.com>
2026-07-04 17:00:27 +0200

    [Add] Agility SDK
    - Some skd versions didn't support tier 2 hardware raytracing making it so the program didn't compile correctly now this is fixed by using the agility sdk

--- Wild/EntryPoint.cpp
+++ Wild/EntryPoint.cpp
@@ -2,6 +2,10 @@
 
 #include <filesystem>
 
+// Run the agility sdk
+extern "C" __declspec(dllexport) extern const unsigned int D3D12SDKVersion = D3D12_AGILITY_SDK_VERSION;
+extern "C" __declspec(dllexport) extern const char* D3D12SDKPath = ".\\D3D12\\";
+
 int main(int, char* argv[])
 {
     Wild::Log::Initialize();
